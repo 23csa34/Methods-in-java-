6491caa751b6ba91b4b407919bb39f101f77061a
Divyabharathi R <23csa34@karpagamtech.ac.in>
2026-06-15 09:49:20 +0530

    Implement simple interest calculation in Java

---
+++ simpleinterest.java
@@ -0,0 +1,24 @@
+//calculate simple interest 
+import java.util.Scanner;
+public class Main {
+    // Function to calculate Simple Interest
+    static double calculateSimpleInterest(double principal,double rate,double time) {
+        
+        
+        
+        return (principal*rate*time)/2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Principal Amount: ");
+        double principal = sc.nextDouble();
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+        System.out.print("Enter Time (in years): ");
+        double time = sc.nextDouble();
+        double si = calculateSimpleInterest(principal, rate, time);
+        System.out.println("Simple Interest = " + si);
+        sc.close();
+    }
+}
